52927b670d1bb8ae09adec8da76e018b8412ba41
Shashank Raj <shashankraj3636@gmail.com>
2023-04-24 21:40:32 +0530

    reports and report user ejs

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,27 +1,28 @@
 const userModel = require('../schemas/userSchema')
 const JWTService = require('../services/JWTService')
 const reportModel = require('../schemas/reporterSchema')
+const postModel = require('../schemas/postSchema')
+const friendsModel = require('../schemas/friendsSchema')
+const reporterModel = require('../schemas/reporterSchema')
+const notificationModel = require('../schemas/notificationSchema')
+const mongoose = require('mongoose')
+
 const getAdmin = async (req, res) => {
   const thistotalaccounts = new Promise((resolve) => resolve(userModel.find()))
-  const thistotalBlockedUsers = new Promise((resolve) =>
-    resolve(userModel.find({ blocked: true })),
-  )
   const thistotalVerifiedUsers = new Promise((resolve) =>
     resolve(userModel.find({ verified: true })),
   )
   let reportsArray
   const thisreports = new Promise((resolve) => resolve(reportModel.find()))
 
-  const [accounts, totalblocked, totalverified, reports] = await Promise.all([
+  const [accounts, totalverified, reports] = await Promise.all([
     thistotalaccounts,
-    thistotalBlockedUsers,
     thistotalVerifiedUsers,
     thisreports,
   ])
 
   return res.render('./Pages/admin', {
     accounts: accounts,
-    totalblocked: totalblocked,
     totalverified: totalverified,
     reports: reports,
   })
@@ -41,12 +42,20 @@ const getReportUser = async (req, res) => {
 
 const getReport = async (req, res) => {
   const reportId = req.params.reportId
+  let rep
   reportModel
     .find({ _id: reportId })
     .then((report) => {
-      res.render('./Pages/report',{
-        report: report[0]
-      })
+      rep = report
+      return report
+    })
+    .then((report) => {
+      userModel.findOne({ stringId: report[0].toUserId }).then((user) => {
+        res.render('./Pages/report', {
+          report: report[0],
+          reporteduser: user,
+        })
+      }) 
     })
     .catch((err) => {
       console.log(err)
@@ -54,10 +63,38 @@ const getReport = async (req, res) => {
     })
 }
 
+const deleteAccount = async (req, res) => {
+  const userId = req.params.deleteId
+  await postModel.deleteMany({ userId })
+  await friendsModel.deleteMany({
+    $or: [{ userId }, { followingUserId: userId }],
+  })
+  await notificationModel.deleteMany({
+    $or: [{ fromId: userId }, { toId: userId }],
+  })
+  await reporterModel.deleteMany({
+    $or: [{ fromUserId: userId }, { toUserId: userId }],
+  })
+  await userModel.findByIdAndDelete(userId)
+  return res.redirect('/admin')
+}
+
+
+const deleteReport = async (req, res) => {
+  const reportId = req.params.reportId;
+  reportModel.findOneAndRemove({_id:reportId}).then(()=>{
+    res.redirect('/admin')
+  }).catch(err=>{
+    console.log(err)
+  })
+}
+
 const adminControllers = {
   getAdmin,
   getReport,
-  getReportUser
+  getReportUser,
+  deleteAccount,
+  deleteReport
 }
 
 module.exports = adminControllers
